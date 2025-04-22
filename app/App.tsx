11678dc0663c7362e0a077925c499c65cb09a142
Muhamed Spahić <muhamed1ism@gmail.com>
2025-04-23 00:13:23 +0200

    [BT-35] Create Topbar

--- app/App.tsx
+++ app/App.tsx
@@ -1,10 +1,11 @@
+import { Layout } from './components/layout/Layout';
 import Root from './routes/root';
 
 function App() {
   return (
-    <>
+    <Layout>
       <Root />
-    </>
+    </Layout>
   );
 }
 
