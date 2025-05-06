emirankorainc/bt-internship-fe

Setup Initial Router

app/routes/root.tsx
@@ -6,6 +6,13 @@ import routeNames from './route-names.tsx';
 import { Contact } from '@app/pages/Contact.tsx';
 import { Home } from '@app/pages/Home.tsx';
 import { Layout } from '@app/components/layout/Layout.tsx';
+import { Buckets } from '@app/pages/Buckets.tsx';
+import { Dashboard } from '@app/pages/Dashboard.tsx';
+import { Evaluation } from '@app/pages/Evaluation.tsx';
+import { Roles } from '@app/pages/Roles.tsx';
+import { Teams } from '@app/pages/Teams.tsx';
+import { Users } from '@app/pages/Users.tsx';
+import { People } from '@app/pages/People.tsx';
 
 const router = createBrowserRouter([
   {
@@ -20,8 +27,36 @@ const router = createBrowserRouter([
         path: routeNames.contact(),
         element: <Contact />,
       },
-    ]
-  }
+      {
+        path: routeNames.buckets(),
+        element: <Buckets />,
+      },
+      {
+        path: routeNames.dashboard(),
+        element: <Dashboard />,
+      },
+      {
+        path: routeNames.evaluation(),
+        element: <Evaluation />,
+      },
+      {
+        path: routeNames.roles(),
+        element: <Roles />,
+      },
+      {
+        path: routeNames.teams(),
+        element: <Teams />,
+      },
+      {
+        path: routeNames.users(),
+        element: <Users />,
+      },
+      {
+        path: routeNames.people(),
+        element: <People />,
+      },
+    ],
+  },
 ]);
 
 export default function Root() {

muhamed1ism/bt-internship

app/routes/root.tsx
@@ -1,17 +1,17 @@
+import { Layout } from '@app/components/layout/Layout.tsx';
+import { Buckets } from '@app/pages/Buckets.tsx';
+import { Contact } from '@app/pages/Contact.tsx';
+import { Evaluation } from '@app/pages/Evaluation.tsx';
+import { Home } from '@app/pages/Home.tsx';
+import { People } from '@app/pages/People.tsx';
+import { Roles } from '@app/pages/Roles.tsx';
+import { Teams } from '@app/pages/Teams.tsx';
+import { Users } from '@app/pages/Users.tsx';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import '../main.css';
 import { queryClient } from '../utils/query-client.ts';
 import routeNames from './route-names.tsx';
-import { Contact } from '@app/pages/Contact.tsx';
-import { Home } from '@app/pages/Home.tsx';
-import { Layout } from '@app/components/layout/Layout.tsx';
-import { People } from '@app/pages/People.tsx';
-import { Buckets } from '@app/pages/Buckets.tsx';
-import { Evaluation } from '@app/pages/Evaluation.tsx';
-import { Teams } from '@app/pages/Teams.tsx';
-import { Users } from '@app/pages/Users.tsx';
-import { Roles } from '@app/pages/Roles.tsx';
 
 const router = createBrowserRouter([
   {