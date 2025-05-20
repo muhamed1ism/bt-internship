d4fdbd4878eb8402a80e5a9552aa9bae2898d1d0
Muhamed Spahić <muhamed1ism@gmail.com>
2025-05-20 21:42:41 +0200

    [BT-20] fix date-picker position

--- app/components/ui/date-picker.tsx
+++ app/components/ui/date-picker.tsx
@@ -33,7 +33,7 @@ export function DatePicker({ date, setDate, className }: DatePickerProps) {
         </Button>
       </PopoverTrigger>
 
-      <PopoverContent className="w-auto rounded-xl p-0" align="start">
+      <PopoverContent className="z-0 w-auto rounded-xl p-0" align="start">
         <Calendar
           mode="single"
           selected={date}
